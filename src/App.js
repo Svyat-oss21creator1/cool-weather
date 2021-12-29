f57329b9c04668e62f59741b8cdfe7f8bbd06ba9
DESKTOP-9RDSU2J\chan <svyatoslavcherepinsky@gmail.com>
2021-12-29 21:51:50 +0200

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+const API_KEY = "d65b6c869af229c32dbee8d4412c6f78";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App  extends React.Component {
+  state ={
+    temp:undefined,
+    city:undefined,
+    country:undefined,
+    sunset:undefined,
+    error:undefined,
+    pressure:undefined
+  }
+
+  gettingWeather =  async (e) =>{
+    e.preventDefault();
+    var city = e.target.elements.city.value;
+    
+    if (city ){ 
+      const api_url = await
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);  
+      const data = await api_url.json(); 
+    
+    var sunset = data.sys.sunset;
+    var date = new Date();
+    date.setTime(sunset);
+    var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    
+    this.setState({
+      temp:data.main.temp,
+      city:data.name,
+      country:data.sys.country,
+      pressure:data.main.pressure,
+      sunset:sunset_date,
+      error:undefined
+    });
+  }else{
+    this.setState({  
+      temp:undefined,
+      city:undefined,
+      country:undefined,
+      pressure:undefined,
+      sunset:undefined,       
+      error:"Введите название города"      
+    });
+  }
+  }
+  render(){
+    return(
+      <div className="wrapper">
+        <div className="main">
+        <div className ="container">
+          <div className="row">
+            <div className="col-sm-5 info">
+            <Info/>
+             </div>
+            <div className="col-sm-7 form ">
+              <Form  weatherMethod ={this.gettingWeather}/>
+        <Weather
+        temp ={this.state.temp}
+        city ={this.state.city}
+        country ={this.state.country}
+        sunset ={this.state.sunset}
+        pressure={this.state.pressure}
+        error ={this.state.error}
+        />        
+        </div>
+        </div>
+        </div>        
+      </div>
+      </div>
+    );
+  }
 }
-
-export default App;
+export default  App;
---
+++ src/components/info.js
@@ -0,0 +1,13 @@
+import React from "react";
+  class Info extends  React.Component{
+      render(){
+          return(
+              <div>
+                  <h2>Ваша погода</h2>
+                  <p>Проверить погоду
+                  </p>
+              </div>
+          );
+      }
+  }
+   export default Info;
